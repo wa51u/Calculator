d52a0bff67d8ed796d5feed66eee320ce43f0d26
Patryk <wa51u6@gmail.com>
2023-01-30 05:59:32 +0100

    first working version

--- script.js
+++ script.js
@@ -2,6 +2,11 @@ const numContainer = document.querySelector("#numContainer")
 //screenSecondLine
 let screenFirstValue = "one"
 let screenSecondValue = "two"
+let numA = []
+let numB = []
+let signType = 0
+
+
 
 for (i=9; i>= 0; i-- ) {
     const button = document.createElement("button")
@@ -52,16 +57,16 @@ function divide(...arg){
 
 function operate(sign,...arg){
     switch (sign) {
-        case "add":
+        case "+":
             return add(...arg);
             break;
-        case "subtract":
+        case "-":
             return subtract(...arg);
             break;
-        case "multiply":
+        case "*":
             return multiply(...arg);
             break;
-        case "divide":
+        case "/":
             return divide(...arg);
             break;
     }
@@ -73,31 +78,64 @@ for ( let i = 0; i <= 9;i++ ){
 let tempName = (`numNr${i}`)
 tempName = document.getElementById(i)
 tempName.addEventListener("click", () => {
-    console.log(i)
-    displayValue += i;
+  if (signType === 0){
+    numA.push(i)
+    console.log(numA.join(""))
+    screenFirstLine.innerText = (numA.join(""))
+  } else {
+    numB.push(i)
+    console.log(numB)
+    screenFirstLine.innerText = (`${(numA.join(""))} ${signType} ${(numB.join(""))}`)
+
+  }
 });
 }
 
 let allSigns = document.getElementsByClassName("sign")
 
 for (let sign of allSigns){
-   // console.log(sign.id)
+ 
     sign.addEventListener("click", () => {
-        console.log(sign.id)
-        displayValue += sign.id;
-        console.log(displayValue);
-    });
-}
+
+        console.log(sign.textContent)
+    if (sign.id === "clear"){
+            clear(1)
+            } else {
+    if (signType === 0 ){
+        signType = sign.textContent
+        screenFirstLine.innerText = (`${(numA.join(""))} ${sign.textContent}`)
+        console.log(signType)
+        
+    }  else {
+        if (sign = "equal"){
+            numA = numA.join("")
+            numB = numB.join("")
+            numA = parseInt(numA)
+            numB = parseInt(numB)
+            screenSecondLine.innerText = (operate(signType,numA,numB))
+            clear()
+        }
+    
+    }
+    }});
+        }
 
 //screenFirstLine 
 let screenFirstLine = document.getElementById("screenFirstLine")
-screenFirstLine.innerText = "test"
-console.log(screenFirstLine.innerText)
+//screenFirstLine.innerText = "test"
+//console.log(screenFirstLine.innerText)
 
 //screenSecondLine
 let screenSecondLine = document.getElementById("screenSecondLine")
-screenSecondLine.innerText = "test2"
-console.log(screenSecondLine.innerText)
+//screenSecondLine.innerText = "test2"
+//console.log(screenSecondLine.innerText)
 
-
-
+function clear(hard){
+    numA = []
+    numB = []
+    signType = 0
+    screenFirstLine.innerText = ""  
+    if (hard == true){
+    screenSecondLine.innerText = "" 
+    }
+} 
